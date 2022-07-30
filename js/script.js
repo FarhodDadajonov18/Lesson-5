bba457457557dad77b9b0fb8b2cbbc4b430fa248
dadajonovfarhod <dadajonovfarhod5@gmail.com>
2022-07-30 09:41:47 +0500

    created ne js about alert and so on

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+var info = alert("Iltimos har xil narsalarni yozib tashlamang");
+firstName = prompt("Ismingizni kiriting...").trim(),
+lastName = prompt ("Familyangizni kiriting").trim(),
+birthDate = prompt ("Tug'ilgan kuningizni kiriting").trim(),
+email= prompt (" Foydalanuvchining email pochtasi").trim(),
+address= prompt ( "Foydalanuvchining manzili").trim(),
+userNum = prompt("Foydalanuvchining nomeri").trim(),
+workPlace = prompt ("Foydalanuvchining ish joyi").trim(),
+profession =prompt ("Foydalanuvchining kasbi").trim(),
+hobby =prompt ("Foydalanuvchining sevimli mashg'uloti").trim(),
+car = prompt("Foydalanuvchining  moshinasi").trim(),
+text = alert ("Malumotiz uchun rahmat");
+
+
+
+
+
+var paragraph = document.createElement("h2");
+
+paragraph.textContent=`Your name is : ${firstName},
+                       Your last name is : ${lastName},
+                       Your birthdate : ${birthDate},
+                       Your Email : ${email},
+                       Your address : ${address},
+                       Your Number : ${userNum},
+                       Your workplace : ${workPlace},
+                       Your profession : ${profession},
+                       Your hobby : ${hobby},
+                       Your avtomobile : ${car}
+                       `;
+
+document.body.style.backgroundColor="black";
+document.body.appendChild(paragraph).style.backgroundColor="brown";
+document.body.appendChild(paragraph).style.color="white";
